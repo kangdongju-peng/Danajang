48761dad716e54e27ab6712bc72df434ffb95b5a
kangdongju-peng <kangdongju05@gmail.com>
2020-08-22 21:27:25 +0900

    4

--- app/src/main/java/com/example/danajang/SharedPreference.kt
+++ app/src/main/java/com/example/danajang/SharedPreference.kt
@@ -1,6 +1,10 @@
 package com.example.danajang
 
 import android.content.Context
+import org.json.JSONArray
+import org.json.JSONException
+import java.lang.reflect.Array.get
+import java.nio.file.Paths.get
 
 class SharedPreference(context : Context) {
     //SharedPreference 데이터 저장하는곳
@@ -18,5 +22,37 @@ class SharedPreference(context : Context) {
         //값 가져오는 함수
         val prefs = context.getSharedPreferences(PREFS_FILENAME, Context.MODE_PRIVATE)
         return prefs.getString(key, "")
+    }
+    fun setArrayList(context: Context,key: String, values : ArrayList<String>) {
+        //리스트를 저장하는 함수, 단어장을 저장할때 쓰일듯
+        val editor = context.getSharedPreferences(PREFS_FILENAME, Context.MODE_PRIVATE).edit()
+        val a =  JSONArray()
+
+        for(i in 0 until values.size step 1)
+            a.put(values[i])
+        if(values.isNotEmpty())
+            editor.putString(key, a.toString())
+         else
+            editor.putString(key, null)
+
+        editor.apply()
+    }
+    fun getArrayList(context: Context,key: String) : ArrayList<String> {
+        ////리스트를 불러오는 함수, 단어장을 저장할때 쓰일듯
+        val prefs = context.getSharedPreferences(PREFS_FILENAME, Context.MODE_PRIVATE)
+        val json = prefs.getString(key,null)
+        var urls = ArrayList<String>()
+        if(json != null){
+            try{
+                val a = JSONArray(json)
+                for(i in 0 until a.length() step 1){
+                    var url = a.optString(i)
+                    urls.add(url)
+                }
+            } catch (e: JSONException){
+                e.printStackTrace()
+            }
+        }
+        return urls
     }
 }
