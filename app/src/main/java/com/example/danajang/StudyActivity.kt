6af6c40c466e0aab8a4bd928cc4b98bc929ad140
kangdongju-peng <kangdongju05@gmail.com>
2020-08-23 22:17:52 +0900

    5일차, 프레그먼트 추가할수있늗데 콘테이너안에서만 추가가 가능함..

--- app/src/main/java/com/example/danajang/StudyActivity.kt
+++ app/src/main/java/com/example/danajang/StudyActivity.kt
@@ -2,21 +2,44 @@ package com.example.danajang
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.viewpager.widget.PagerAdapter
 import kotlinx.android.synthetic.main.activity_study.*
 
 class StudyActivity: AppCompatActivity() {
     //단어 공부하는 프레그먼트 페이지
 
+    val FRAGMENT_TAG = "FRAGMENT_TAG"
+    val KEY_NUMBER = "KEY_NUMBER"
+    var mNumber = 0
+    val mListener = object: FragmentManager.OnBackStackChangedListener {
+        override fun onBackStackChanged() {
+            var fragmentManager = getSupportFragmentManager()
+            var count = 0
+            for (f in fragmentManager.getFragments()){
+                if (f != null)
+                    count++
+            }
+            mNumber = count
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_study)
+        val sp = SharedPreference(this )
+        val WORLD_COUNT = "world count"
         // 어댑터 생성 <어댑터는 여러 프래그먼트를 만들고 관리함>
-        val Adapter = PageAdapter(supportFragmentManager)
+        //val Adapter = PageAdapter(supportFragmentManager)
         // 뷰 페이저에 어댑터 연결
-        view_pager.adapter = Adapter
-        view_pager.currentItem=0
+        //view_pager.adapter = Adapter
+        //view_pager.currentItem=0
         // 탭 레아아웃에 뷰페이저 연결
+        for(i in 1 until sp.getString(WORLD_COUNT)!!.toInt() step 1){
+            var fragmentManager = getSupportFragmentManager()
+            //fragmentManager.beginTransaction()
+              //  .add(R.id.)
+        }
+
 
 
 
