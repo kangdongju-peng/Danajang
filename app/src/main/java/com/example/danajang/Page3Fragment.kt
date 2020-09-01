cab11fa40d7a13aa1cd0fa61a8bd18ead7b925bd
kangdongju-peng <kangdongju05@gmail.com>
2020-09-01 22:00:08 +0900

    프래그먼트에 이미지 표시하기(2)

--- app/src/main/java/com/example/danajang/Page3Fragment.kt
+++ app/src/main/java/com/example/danajang/Page3Fragment.kt
@@ -1,10 +1,13 @@
 package com.example.danajang
 
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_1.*
 
 class Page3Fragment : Fragment() {
     override fun onCreateView(
@@ -15,6 +18,9 @@ class Page3Fragment : Fragment() {
         val world_list : ArrayList<String>
         world_list = (getActivity() as SharedPreference).getArrayList(context!!,SharedPreference.voca_name) //단어 리스트를 가져오ㅁ
         //image = world_list[(getActivity() as LoadAdapter).getCount()]    <-- 이미지를 월드리스트에서 가져옴
+        var uri = Uri.parse("file:///" + Environment.getExternalStorageDirectory() + world_list[(getActivity() as LoadAdapter).getCount()])
+        image_view.setImageURI(uri) // 이미지 표시하기
+
         return root
     }
     // 뷰 생성이 완료되면 호출되는 메소드
